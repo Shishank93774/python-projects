23f67c792704118a4faea5eadee5fe2a3cc21895
Shishank <shishankrawat93774@gmail.com>
2024-01-16 17:28:17 +0530

    small update

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -1,6 +1,6 @@
 import math
 import time
-from main import HumanPlayer, RandomComputerPlayer, SmartComputerPlayer
+from player import HumanPlayer, RandomComputerPlayer, SmartComputerPlayer
 
 
 class TicTacToe():
@@ -91,12 +91,12 @@ def play(game, x_player, o_player, print_game=True):
         time.sleep(.8)
 
     if print_game:
-        print('It\'s a tie!')
+        print('It\'s a tie!, Well played')
 
 
 
 if __name__ == '__main__':
-    x_player = SmartComputerPlayer('X')
-    o_player = HumanPlayer('O')
+    x_player = HumanPlayer('X')
+    o_player = SmartComputerPlayer('O')
     t = TicTacToe()
     play(t, x_player, o_player, print_game=True)
